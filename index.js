3e09923ae87b67d299c378667661ba12cd60f0a5
Fabio Santo <fabio.santo.mail@gmail.com>
2018-05-28 07:21:12 +1000

    add hostname check

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ const server = http.createServer((req, res) => {
     var request = url.parse(req.url, true);
     var action = request.pathname;
 
-    var hostname = req.headers.host;
-    console.log(hostname)
-    if (!hostname.contains('alessiafrancischiello.com'))
-        req.end();
-
     // var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
     // if (ip == '127.0.0.1')
     //     req.end();
@@ -24,14 +19,26 @@ const server = http.createServer((req, res) => {
         res.write("Hi, you are number " + count + " visitors");
         res.end();
     } else {
-        var img = fs.readFileSync('./image.jpg');
-        res.writeHead(200, {'Content-Type': 'image/jpeg'});
-        res.end(img, 'binary');
-        count++;
+        var hostname = req.headers.host
+        var origin = req.headers.origin
+        console.log('host is ' + hostname)
+        console.log('origin is ' + origin)
+
+        if (hostname == 'alessiafrancischiello.com' ||
+            origin == 'alessiafrancischiello.com') {
+            var img = fs.readFileSync('./image.jpg');
+            res.writeHead(200, {'Content-Type': 'image/jpeg'});
+            res.end(img, 'binary');
+            count++;
+        } else {
+            res.end();
+        }
+
+
     }
 
 });
 
-server.listen(port, function() {
+server.listen(port, function () {
     console.log("App is running on port " + port);
 });
